aa6aaf31fff7121605c3ab9682e08c1436be1e08
Evan Relf <evan@evanrelf.com>
2023-07-16 23:56:58 -0700

    Add `Echo` and `EchoOk` messages for challenge 1

--- src/HotGoss/Challenge1.hs
+++ src/HotGoss/Challenge1.hs
@@ -1,4 +1,62 @@
 module HotGoss.Challenge1 (main) where
 
+import Data.Aeson
+import Data.Aeson.Types (Parser)
+import HotGoss.Protocol
+import HotGoss.Union
+
+data Echo = Echo
+  { messageId :: Word
+  , echo :: Text
+  }
+  deriving stock (Show)
+
+instance ToJSON Echo where
+  toJSON :: Echo -> Value
+  toJSON echo =
+    object
+      [ "type" .= ("echo" :: Text)
+      , "msg_id" .= echo.messageId
+      , "echo" .= echo.echo
+      ]
+
+instance FromJSON Echo where
+  parseJSON :: Value -> Parser Echo
+  parseJSON =
+    withObject "Echo" \o -> do
+      type_ :: Text <- o .: "type"
+      guard (type_ == "echo")
+      messageId <- o .: "msg_id"
+      echo <- o .: "echo"
+      pure Echo{ messageId, echo }
+
+data EchoOk = EchoOk
+  { messageId :: Word
+  , inReplyTo :: Word
+  , echo :: Text
+  }
+  deriving stock (Show)
+
+instance ToJSON EchoOk where
+  toJSON :: EchoOk -> Value
+  toJSON echoOk =
+    object
+      [ "type" .= ("echo_ok" :: Text)
+      , "msg_id" .= echoOk.messageId
+      , "in_reply_to" .= echoOk.inReplyTo
+      , "echo" .= echoOk.echo
+      ]
+
+instance FromJSON EchoOk where
+  parseJSON :: Value -> Parser EchoOk
+  parseJSON =
+    withObject "EchoOk" \o -> do
+      type_ :: Text <- o .: "type"
+      guard (type_ == "echo_ok")
+      messageId <- o .: "msg_id"
+      inReplyTo <- o .: "in_reply_to"
+      echo <- o .: "echo"
+      pure EchoOk{ messageId, inReplyTo, echo }
+
 main :: IO ()
 main = pure ()
--- src/HotGoss/Protocol.hs
+++ src/HotGoss/Protocol.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-
 module HotGoss.Protocol
   ( Message (..)
   , Init (..)
