38332ac8b009a89b05567539fd42034f1a7b8cda
Evan Relf <evan@evanrelf.com>
2023-07-16 22:12:02 -0700

    Ditch `MessageBody`

--- src/HotGoss/Protocol.hs
+++ src/HotGoss/Protocol.hs
@@ -1,6 +1,5 @@
 module HotGoss.Protocol
   ( Message (..)
-  , MessageBody (..)
   )
 where
 
@@ -10,7 +9,7 @@ import Data.Aeson.Types (Parser)
 data Message a = Message
   { source :: Text
   , destination :: Text
-  , body :: MessageBody a
+  , body :: a
   }
 
 instance ToJSON a => ToJSON (Message a) where
@@ -30,35 +29,3 @@ instance FromJSON a => FromJSON (Message a) where
       destination <- o .: "dest"
       body <- o .: "body"
       pure Message{ source, destination, body }
-
-data MessageBody a = MessageBody
-  { kind :: Text
-  , messageId :: Maybe Int
-  , inReplyTo :: Maybe Int
-  , fields :: a
-  }
-
-instance ToJSON a => ToJSON (MessageBody a) where
-  toJSON :: MessageBody a -> Value
-  toJSON messageBody =
-    object
-      [ "type" .= messageBody.kind
-      , "msg_id" .= messageBody.messageId
-      , "in_reply_to" .= messageBody.inReplyTo
-      ]
-    `unsafeUnion`
-    toJSON messageBody.fields
-    where
-    unsafeUnion :: Value -> Value -> Value
-    unsafeUnion (Object l) (Object r) = Object (l <> r)
-    unsafeUnion _ _ = error "unsafeUnion given non-object(s)"
-
-instance FromJSON a => FromJSON (MessageBody a) where
-  parseJSON :: Value -> Parser (MessageBody a)
-  parseJSON =
-    withObject "MessageBody" \o -> do
-      kind <- o .: "type"
-      messageId <- o .:? "msg_id"
-      inReplyTo <- o .:? "in_reply_to"
-      fields <- parseJSON (Object o)
-      pure MessageBody{ kind, messageId, inReplyTo, fields }
