9b741a335c154274aa53b78253faf52f41681bb7
Evan Relf <evan@evanrelf.com>
2023-07-16 23:50:08 -0700

    Package `maelstrom`

---
+++ maelstrom.nix
@@ -0,0 +1,28 @@
+let
+  pkgs = import <nixpkgs> { };
+
+in
+pkgs.stdenv.mkDerivation {
+  pname = "maelstrom";
+  version = "0.2.3";
+  src =
+    builtins.fetchTarball {
+      url = "https://github.com/jepsen-io/maelstrom/releases/download/v0.2.3/maelstrom.tar.bz2";
+      sha256 = "sha256:1hkczlbgps3sl4mh6hk49jimp6wmks8hki0bqijxsqfbf0hcakwq";
+    };
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/bin/
+    mv lib $out/lib
+    cat <<EOF > $out/bin/maelstrom
+      exec ${pkgs.jdk}/bin/java \
+        -Djava.awt.headless=true \
+        -jar "$out/lib/maelstrom.jar" \
+        "\$@"
+    EOF
+    chmod +x $out/bin/maelstrom
+    wrapProgram $out/bin/maelstrom \
+      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jdk ]}
+  '';
+}
