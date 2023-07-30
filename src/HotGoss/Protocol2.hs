cc51718a74c14638a3ee11f97cdead2d01b135b6
Evan Relf <evan@evanrelf.com>
2023-07-29 17:12:38 -0700

    wip2

--- src/HotGoss/Protocol2.hs
+++ src/HotGoss/Protocol2.hs
@@ -24,14 +24,12 @@ import Data.Text.Display (Display)
 import Deriving.Aeson
 import GHC.Generics (Rep)
 import HotGoss.ErrorCode (ErrorCode)
-import Prelude hiding (All, Compose)
+import Prelude hiding (All, Compose, state)
 import Text.Show (Show (..))
 
-import qualified Data.Aeson as Aeson (Result (..))
 import qualified Data.Aeson.KeyMap as KeyMap
 import qualified Data.ByteString.Lazy as LByteString
 import qualified Data.Data as Data
-import qualified Data.Text.IO as Text
 import qualified UnliftIO.Exception as Exception
 
 data Message o a = Message
@@ -113,20 +111,18 @@ instance Show Omitted where
   show _ = "Omitted"
 
 data ProtocolException
-  = FailedToInit
-  | FailedToDecode String
-  | UnexpectedMessage Value String
+  = FailedToDecode String
   deriving stock (Show)
   deriving anyclass (Exception)
 
-sendJson :: (ToJSON a, MonadIO m) => a -> m ()
-sendJson message = do
+readJson :: (FromJSON a, MonadIO m) => m (Either String a)
+readJson = eitherDecode' . encodeUtf8 <$> getLine
+
+writeJson :: (ToJSON a, MonadIO m) => a -> m ()
+writeJson message = do
   let bytes = encode message <> "\n"
   liftIO $ LByteString.hPut stdout bytes
   hFlush stdout
-
-receiveJson :: (FromJSON a, MonadIO m) => m (Either String a)
-receiveJson = eitherDecode' . encodeUtf8 <$> getLine
 
 sendTo :: NodeId -> a -> Message Endo a
 sendTo dest body =
@@ -171,71 +167,76 @@ data Error = Error
 
 data Handler s i where
   Handler
-    :: All ToJSON xs
-    => (s -> Message Exo i -> (s, NP (Message Endo) xs))
+    :: All ToJSON os
+    => (s -> Message Exo i -> (s, NP (Message Endo) os))
     -> Handler s i
 
-data Handlers s xs where
-  Handlers
-    :: All (Compose FromJSON (Message Exo)) xs
-    => NP (Handler s) xs
-    -> Handlers s xs
-
 node
-  :: forall s xs m
-   . MonadIO m
+  :: forall s is m
+   . (All (Compose FromJSON (Message Exo)) is, MonadIO m)
   => s
-  -> Handlers s xs
+  -> NP (Handler s) is
   -> m ()
-node state (Handlers handlers) = do
+node initialState handlers = do
+  let receive :: FromJSON a => m a
+      receive = do
+        readJson >>= \case
+          Left err -> Exception.throwIO $ FailedToDecode err
+          Right req -> pure req
+
   getMessageId <- do
     ref <- newIORef 1
     pure $ atomicModifyIORef' ref \x -> (x + 1, MessageId x)
 
-  (nodeId, nodeIds) <-
-    receiveJson @(Message Exo Init) >>= \case
-      Left err -> Exception.throwIO FailedToInit
-      Right req -> do
-        msgId <- getMessageId
-        let res = replyTo req
-              InitOk
-                { msgId
-                , inReplyTo = req.body.msgId
-                }
-        sendJson res
-        pure (req.body.nodeId, req.body.nodeIds)
+  -- Init
+  (nodeId, nodeIds) <- do
+    req <- receive @(Message Exo Init)
+    msgId <- getMessageId
+    let res = replyTo req
+          InitOk
+            { msgId
+            , inReplyTo = req.body.msgId
+            }
+    writeJson res
+    pure (req.body.nodeId, req.body.nodeIds)
 
-  state & fix \loop s -> do
-    json <-
-      receiveJson @Value >>= \case
-        Left err -> Exception.throwIO $ FailedToDecode err
-        Right json -> pure json
+  -- Main loop
+  initialState & fix \loop state -> do
+    req <- receive @(NS (Message Exo) is)
+    msgId <- getMessageId
+    let (state', ress) = runHandlers handlers state req
+    traverse_ writeJson ress
+    loop state'
 
-    case fromJSON @(NS (Message Exo) xs) json of
-      Aeson.Error err -> Exception.throwIO $ UnexpectedMessage json err
-      Success message -> do
-        msgId <- getMessageId
-        let s' :: s
-            -- ress :: All ToJSON xs => NP (Message Endo) xs
-            (s', ress) = undefined s
-        undefined
-        -- ctraverse__NP (Proxy @ToJSON) sendJson ress
-        loop s'
+runHandlers
+  :: forall s is
+   . SListI is
+  => NP (Handler s) is
+  -> s
+  -> NS (Message Exo) is
+  -> (s, [Message Endo Value])
+runHandlers handlers state message = collapse_NS $ ap_NS handlerFns message
+  where
+  handlerFns :: NP (Message Exo -.-> K (s, [Message Endo Value])) is
+  handlerFns =
+    map_NP
+      (\handler -> Fn \message -> K $ runHandler handler state message)
+      handlers
 
-step :: Handler s i -> s -> Message Exo i -> (s, [Message Endo Value])
-step (Handler handler) state message =
-  let
-    (state', np) = handler state message
-    values = collapse_NP $ cmap_NP (Proxy @ToJSON) (K . fmap toJSON) $ np
-  in
-    (state', values)
-
+  runHandler :: Handler s i -> s -> Message Exo i -> (s, [Message Endo Value])
+  runHandler (Handler handler) state message =
+    let
+      (state', np) = handler state message
+      values = collapse_NP $ cmap_NP (Proxy @ToJSON) (K . fmap toJSON) np
+    in
+      (state', values)
 
 deriving newtype instance ToJSON a => ToJSON (I a)
 
 deriving newtype instance FromJSON a => FromJSON (I a)
 
 instance All (Compose FromJSON f) xs => FromJSON (NS f xs) where
+  parseJSON :: Value -> Parser (NS f xs)
   parseJSON v =
       unK
     $ traverse__NP (mapKK sequence'_NS)
